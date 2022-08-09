7f7ec73131c65f4c10550a56634e070bc8d980ad
seid hamid <seidhamid200@gmail.com>
2022-08-09 12:37:33 -0700

    printff

--- _printf.c
+++ _printf.c
@@ -1,47 +1,42 @@
 #include "main.h"
 /**
- * _printf - prints formatted data to stdout
- * @format: string that contains the format to print
- * Return: number of characters written
+ * _printf - printf function
+ * @format: const char pointer
+ * Return: b_len
  */
-int _printf(char *format, ...)
+int _printf(const char *format, ...)
 {
-int written = 0, (*structype)(char *, va_list);
-char q[3];
-va_list pa;
-if (format == NULL)
+int (*pfunc)(va_list, flags_t *);
+const char *p;
+va_list arguments;
+flags_t flags = {0, 0, 0};
+register int count = 0;
+va_start(arguments, format);
+if (!format || (format[0] == '%' && !format[1]))
 return (-1);
-q[2] = '\0';
-va_start(pa, format);
+if (format[0] == '%' && format[1] == ' ' && !format[2])
+return (-1);
+for (p = format; *p; p++)
+{
+if (*p == '%')
+{
+p++;
+if (*p == '%')
+{
+count += _putchar('%');
+continue;
+}
+while (get_flag(*p, &flags))
+p++;
+pfunc = get_print(*p);
+count += (pfunc)
+? pfunc(arguments, &flags)
+: _printf("%%%c", *p);
+}
+else
+count += _putchar(*p);
+}
 _putchar(-1);
-while (format[0])
-{
-if (format[0] == '%')
-{
-structype = driver(format);
-if (structype)
-{
-q[0] = '%';
-q[1] = format[1];
-written += structype(q, pa);
+va_end(arguments);
+return (count);
 }
-else if (format[1] != '\0')
-{
-written += _putchar('%');
-written += _putchar(format[1]);
-}
-else
-{
-written += _putchar('%');
-break;
-}
-format += 2;
-}
-else
-{
-written += _putchar(format[0]);
-format++;
-}
-}
-_putchar(-2);
-return (written); }
