402e0babfb78e5d2e0f4b0b904c42953a6a3fec5
seid hamid <seidhamid200@gmail.com>
2022-08-09 05:24:02 -0700

    main.h

--- _putchar.c
+++ _putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "mainn.h"
 
 /**
  * _putchar - writes the character c to stdout
@@ -10,6 +10,6 @@
 int _putchar(char c)
 {
 
-  return (write(1, &c, 1));
+return (write(1, &c, 1));
 
 }
--- main.h
+++ main.h
@@ -1,22 +1,47 @@
-#ifndef HEADER_MAIN
-#define HEADER_MAIN
+#ifndef HOLBERTON_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
-int _putchar(char c);
-void _puts(char *str);
+#include <limits.h>
+#include <unistd.h>
+
+/**
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+char *id;
+int (*f)();
+} convert_match;
+
+int printf_pointer(va_list val);
+int printf_hex_aux(unsigned long int num);
+int printf_HEX_aux(unsigned int num);
+int printf_exclusive_string(va_list val);
+int printf_HEX(va_list val);
+int printf_hex(va_list val);
+int printf_oct(va_list val);
+int printf_unsigned(va_list args);
+int printf_bin(va_list val);
+int printf_srev(va_list args);
+int printf_rot13(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
 int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int _strlenc(const char *s);
+int printf_37(void);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _putchar(char c);
 int _printf(const char *format, ...);
-char *convert_to(char representation[], unsigned int num, int base);
-int function_manager(char c, va_list arg);
-int _switch(char c, va_list arg);
-int print_character(va_list arg);
-int print_sign(va_list arg, int base);
-int print_unsign(va_list arg, int base);
-int print_string(va_list arg);
-int print_ptr(va_list arg);
-int print_rot13(va_list arg);
-int print_rev(va_list arg);
-int print_base16_upper_lower(va_list arg, char *representation);
-int loop_format(va_list arg, const char *string);
-int call_function_manager(char aux, va_list arg);
-int check_percent(int *flag, char aux);
+
 #endif
